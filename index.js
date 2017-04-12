3712bee1623e84dafa4d221a3f214b4dc618776a
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-12 10:48:16 +0800

    initial commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 const path = require('path')
 const ejsLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
-const todo = require('./routes/todo_router')
+// const todo = require('./routes/todo_router')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-mongoose.connect('mongodb://localhost/myapp')
+// mongoose.connect('mongodb://localhost/myapp')
 
 mongoose.Promise = global.Promise
 
@@ -21,10 +21,12 @@ app.use(ejsLayouts)
 
 app.set('view engine', 'ejs')
 
-app.use('/todo', todo)
+// app.use('/todo', todo)
 
 app.get('/', (req, res) => {
   res.send('Vell, vell, vell. Vellcome to my app.')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function () {
+  console.log('app is running')
+})
